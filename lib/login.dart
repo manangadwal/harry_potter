81a1540a5f5d6980b0353147e590eddd834cc31e
Manan Gadwal <manan2000gadwal@gmail.com>
2021-07-31 18:17:33 +0530

    Initial commit

---
+++ lib/login.dart
@@ -0,0 +1,136 @@
+// import 'package:first_day/pages/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _textcontroler = new TextEditingController();
+  final TextEditingController _textcontroler2 = new TextEditingController();
+  var value = "";
+  bool cb = false;
+  bool c = true;
+  final _formkey = GlobalKey<FormState>();
+  movetohome(BuildContext context) async {
+    if (_formkey.currentState.validate()) {
+      setState(() {
+        cb = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      // await Navigator.pushNamed(context, MyRoutes.homeRoute);
+      setState(() {
+        cb = false;
+        _textcontroler.clear();
+        _textcontroler2.clear();
+        c = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Opacity(
+      opacity: 0.9,
+      child: Material(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white,
+        child: Form(
+          key: _formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TextFormField(
+                      style: TextStyle(color: Colors.black),
+                      decoration: InputDecoration(
+                          labelText: "Username",
+                          hintText: " Enter username",
+                          hintStyle: TextStyle(
+                              fontSize: 7,
+                              fontWeight: FontWeight.w100,
+                              color: Colors.white)),
+                      onChanged: (name) {
+                        value = name;
+                        setState(() {});
+                        if (c = false) {
+                          name = "";
+                        }
+                        setState(() {});
+                      },
+                      validator: (value) {
+                        if (value != "ADMIN") {
+                          return "wrong username or password";
+                        }
+
+                        return null;
+                      },
+                      controller: _textcontroler,
+                    ),
+                    TextFormField(
+                      style: TextStyle(color: Colors.black),
+                      obscureText: true,
+                      obscuringCharacter: "*",
+                      decoration: InputDecoration(
+                          labelText: "Password",
+                          hintText: " Enter password",
+                          hintStyle: TextStyle(
+                              fontSize: 10,
+                              fontWeight: FontWeight.w100,
+                              color: Colors.white)),
+                      validator: (value) {
+                        if (value != "12345678") {
+                          return "wrong username or password";
+                        }
+
+                        return null;
+                      },
+                      controller: _textcontroler2,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Material(
+                      borderRadius: BorderRadius.circular(cb ? 50 : 5),
+                      color: Colors.orange,
+                      child: InkWell(
+                        // splashColor: Colors.amber,
+                        enableFeedback: true,
+                        onTap: () => movetohome(context),
+                        child: AnimatedContainer(
+                          duration: Duration(seconds: 1),
+                          height: cb ? 60 : 50,
+                          width: cb ? 60 : 120,
+                          child: Center(
+                            child: cb
+                                ? Icon(Icons.done, color: Colors.white)
+                                : Text(
+                                    "Login",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+//@dart=2.9
+import 'package:flutter/material.dart';
+import 'package:harry_potter/login.dart';
+import 'package:video_player/video_player.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(myVideo());
+}
+
+class myVideo extends StatefulWidget {
+  const myVideo({Key key}) : super(key: key);
+
+  @override
+  _myVideoState createState() => _myVideoState();
+}
+
+class _myVideoState extends State<myVideo> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset("lib/assets/1.mp4")
+      ..initialize().then((_) {
+        _controller.play();
+        _controller.setLooping(true);
+        _controller.setVolume(0);
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Cinzel Decorative'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            Container(child: VideoPlayer(_controller)),
+            Center(
+              child: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        "POTTER  HEADS",
+                        style: TextStyle(
+                            fontSize: 25,
+                            fontWeight: FontWeight.bold,
+                            // fontStyle: FontStyle.italic,
+                            color: Colors.white,
+                            fontFamily: 'Cinzel Decorative'),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          LoginPage(),
+                          SizedBox(
+                            height: 10,
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
